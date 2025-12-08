72d51910f75708e4cabe89c4416733fe6337314d
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-08 15:53:40 +0530

    basic fetch

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+
+  if (!data) return <div></div>;
 
   return (
     <>
       <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+        {data.map(post => (
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
