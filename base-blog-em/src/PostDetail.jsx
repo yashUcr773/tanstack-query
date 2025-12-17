885b21457914fdd47716b845ed1c5c0f0f5e88fc
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-17 22:42:32 +0530

    delete mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchComments } from './api';
 import './PostDetail.css';
 
-export function PostDetail({ post }) {
+export function PostDetail({ post, deleteMutation }) {
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['comments', post.id],
     queryFn: () => fetchComments(post.id),
@@ -19,7 +19,14 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isPending && <p className="loading">Deleting the post...</p>}
+      {deleteMutation.isError && (
+        <p className="error">Error deleting the post: {deleteMutation.error.toString()} </p>
+      )}
+      {deleteMutation.isSuccess && <p className="success">Post deleted</p>}
+
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map(comment => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { fetchPosts, deletePost, updatePost } from './api';
 import { PostDetail } from './PostDetail';
@@ -10,6 +10,10 @@ export function Posts() {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+
+  const deleteMutation = useMutation({
+    mutationFn: postId => deletePost(postId),
+  });
 
   const { data, isLoading, isFetching, isError, error } = useQuery({
     queryKey: ['posts', currentPage],
@@ -39,7 +43,14 @@ export function Posts() {
     <>
       <ul>
         {data.map(post => (
-          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => {
+              deleteMutation.reset();
+              setSelectedPost(post);
+            }}
+          >
             {post.title}
           </li>
         ))}
@@ -64,7 +75,7 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && <PostDetail post={selectedPost} deleteMutation={deleteMutation} />}
     </>
   );
 }
