72deded54826a0cf792145497205bd1c6a076705
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-17 23:26:15 +0530

    add queryClient

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,19 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import './App.css';
+import { InfinitePeople } from './people/InfinitePeople';
+import { InfiniteSpecies } from './species/InfiniteSpecies';
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      <InfiniteSpecies />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        <InfiniteSpecies />
+      </div>
+    </QueryClientProvider>
   );
 }
 
