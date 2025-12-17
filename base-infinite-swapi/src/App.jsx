a84d453f06629f7ddd2e123b00278d4b46316279
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-18 00:00:32 +0530

    infinite scroll

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { InfinitePeople } from './people/InfinitePeople';
-import { InfiniteSpecies } from './species/InfiniteSpecies';
-import { QueryClient } from '@tanstack/react-query';
+// import { InfiniteSpecies } from './species/InfiniteSpecies';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <h1>Infinite SWAPI</h1>
         <InfinitePeople />
-        <InfiniteSpecies />
+        {/* <InfiniteSpecies /> */}
       </div>
       <ReactQueryDevtools></ReactQueryDevtools>
     </QueryClientProvider>
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,15 +1,44 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.py4e.com/api/people/";
-const fetchUrl = async (url) => {
+const initialUrl = 'https://swapi.py4e.com/api/people/';
+const fetchUrl = async url => {
   const response = await fetch(url);
   return response.json();
 };
 
-fetchUrl(initialUrl).then(d => console.log(d))
-
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError, error } =
+    useInfiniteQuery({
+      queryKey: ['sw-people'],
+      queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      getNextPageParam: lastPage => lastPage.next || undefined,
+    });
+  console.log('🚀 ~ InfinitePeople ~ data:', data);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>{error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className="loading">Fetching...</div>}
+      <InfiniteScroll
+        hasMore={hasNextPage}
+        loadMore={() => {
+          if (!isFetching) fetchNextPage();
+        }}
+      >
+        {data.pages.map(pageData => {
+          return pageData.results.map(person => (
+            <Person
+              key={person.name}
+              name={person.name}
+              eyeColor={person.eye_color}
+              hairColor={person.hair_color}
+            ></Person>
+          ));
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
