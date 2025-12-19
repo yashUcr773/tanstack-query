8c7667bb804c6f1abc7e34083780332690bcccad
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-20 00:59:10 +0530

    prefetch treatments on home page

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import type { Treatment } from '@shared/types';
 
@@ -18,4 +18,9 @@ export function useTreatments(): Treatment[] {
   });
 
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery({ queryKey: [queryKeys.treatments], queryFn: getTreatments });
 }
