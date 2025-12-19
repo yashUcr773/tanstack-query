e9daf3d03672eecae94897cdd80c94c57a15e26f
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-20 00:04:35 +0530

    setup react query provider

--- client/src/react-query/queryClient.ts
+++ client/src/react-query/queryClient.ts
@@ -1,4 +1,6 @@
-import { toast } from "@/components/app/toast";
+import { QueryClient } from '@tanstack/react-query';
+
+import { toast } from '@/components/app/toast';
 
 // function errorHandler(errorMsg: string) {
 //   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
@@ -15,3 +17,5 @@ import { toast } from "@/components/app/toast";
 //     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
 //   }
 // }
+
+export const queryClient = new QueryClient();
