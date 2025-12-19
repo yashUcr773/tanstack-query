076784a6c56e033f7b1fb4a21a8b10f4538ee9f4
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-20 00:16:03 +0530

    get treatments use query

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,21 @@
-import type { Treatment } from "@shared/types";
+import { useQuery } from '@tanstack/react-query';
 
-import { axiosInstance } from "@/axiosInstance";
-import { queryKeys } from "@/react-query/constants";
+import type { Treatment } from '@shared/types';
+
+import { axiosInstance } from '@/axiosInstance';
+import { queryKeys } from '@/react-query/constants';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const { data = [] } = useQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments,
+  });
+
+  return data;
 }
