ea0ffef99000bc2a6ddae35847c6cfabfe12df80
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-21 01:13:15 +0530

    basic mutation for set appointment

--- client/src/components/appointments/hooks/useReserveAppointment.ts
+++ client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,30 +1,37 @@
-import { Appointment } from "@shared/types";
+import { useMutation } from '@tanstack/react-query';
 
-import { useLoginData } from "@/auth/AuthContext";
-import { axiosInstance } from "@/axiosInstance";
-import { useCustomToast } from "@/components/app/hooks/useCustomToast";
-import { queryKeys } from "@/react-query/constants";
+import { Appointment } from '@shared/types';
+
+import { useLoginData } from '@/auth/AuthContext';
+import { axiosInstance } from '@/axiosInstance';
+import { useCustomToast } from '@/components/app/hooks/useCustomToast';
+import { queryKeys } from '@/react-query/constants';
 
 // for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? 'replace' : 'add';
+  const patchData = [{ op: patchOp, path: '/userId', value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 export function useReserveAppointment() {
   const { userId } = useLoginData();
 
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    mutationFn: (appointment: Appointment) => setAppointmentUser(appointment, userId),
+    onSuccess: data => {
+      console.log(data);
+      toast({ title: 'Appointment Scheduled', status: 'success' });
+    },
+  });
+
+  return mutate;
 }
