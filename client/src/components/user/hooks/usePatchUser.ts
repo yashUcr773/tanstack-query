f4e7ce6660d1f7af11fa62c110a0f7e77440fbf0
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-21 01:33:29 +0530

    update query from mutation response. update user data

--- client/src/components/user/hooks/usePatchUser.ts
+++ client/src/components/user/hooks/usePatchUser.ts
@@ -1,37 +1,43 @@
-import jsonpatch from "fast-json-patch";
+import { useMutation } from '@tanstack/react-query';
+import jsonpatch from 'fast-json-patch';
 
-import type { User } from "@shared/types";
+import type { User } from '@shared/types';
 
-import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
-import { useUser } from "./useUser";
+import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
+import { useUser } from './useUser';
+
+import { toast } from '@/components/app/toast';
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData.token),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData.token),
+    }
+  );
+  return data.user;
+}
 
 export function usePatchUser() {
   const { user, updateUser } = useUser();
 
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const { mutate: patchUser } = useMutation({
+    mutationFn: (newData: User) => patchUserOnServer(newData, user),
+    onSuccess: (userData: User) => {
+      updateUser(userData);
+      toast({ title: 'Update successful', status: 'info' });
+    },
+  });
 
   return patchUser;
 }
