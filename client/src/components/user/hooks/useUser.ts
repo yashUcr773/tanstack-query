8e846085dbaff1a5d7eb42f84fca98677751909c
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-21 00:27:53 +0530

    get user data

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -1,26 +1,30 @@
-import { AxiosResponse } from "axios";
+import { useQuery } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 
-import type { User } from "@shared/types";
+import type { User } from '@shared/types';
 
-import { useLoginData } from "@/auth/AuthContext";
-import { axiosInstance, getJWTHeader } from "@/axiosInstance";
-import { queryKeys } from "@/react-query/constants";
+import { useLoginData } from '@/auth/AuthContext';
+import { axiosInstance, getJWTHeader } from '@/axiosInstance';
+import { generateUserKey } from '@/react-query/key-factories';
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(`/user/${userId}`, {
+    headers: getJWTHeader(userToken),
+  });
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+  const { userId, userToken } = useLoginData();
+
+  const { data: user } = useQuery({
+    enabled: !!userId,
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: () => getUser(userId, userToken),
+    staleTime: Infinity,
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
