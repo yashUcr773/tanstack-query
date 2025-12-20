f9b5e4d9aed620e8dd9230352f6ed19fa3512969
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-12-21 00:31:23 +0530

    set and remove query data for logged in user

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import type { User } from '@shared/types';
 
 import { useLoginData } from '@/auth/AuthContext';
 import { axiosInstance, getJWTHeader } from '@/axiosInstance';
+import { queryKeys } from '@/react-query/constants';
 import { generateUserKey } from '@/react-query/key-factories';
 
 // query function
@@ -18,6 +19,7 @@ async function getUser(userId: number, userToken: string) {
 
 export function useUser() {
   const { userId, userToken } = useLoginData();
+  const queryClient = useQueryClient();
 
   const { data: user } = useQuery({
     enabled: !!userId,
@@ -28,12 +30,14 @@ export function useUser() {
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    queryClient.setQueryData(generateUserKey(newUser.id, newUser.token), newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    queryClient.refetchQueries({
+      queryKey: [queryKeys.user],
+    });
   }
 
   return { user, updateUser, clearUser };
